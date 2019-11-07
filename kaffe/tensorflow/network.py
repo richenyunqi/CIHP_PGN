8ebbc28d5f3801a839933db47a8f064c7d46047b
richenyunqi <csjiangfeng@gmail.com>
2019-11-07 13:22:07 +0800

    适应Python3

--- kaffe/tensorflow/network.py
+++ kaffe/tensorflow/network.py
@@ -133,7 +133,7 @@ class Network(object):
         # Convolution for a given input and kernel
         convolve = lambda i, k: tf.nn.conv2d(i, k, [1, s_h, s_w, 1], padding=padding)
         with tf.variable_scope(name) as scope:
-            kernel = self.make_w_var('weights', shape=[k_h, k_w, int(c_i) / group, c_o])
+            kernel = self.make_w_var('weights', shape=[int(k_h), int(k_w), int(c_i) / int(group), int(c_o)])
             if group == 1:
                 # This is the common-case. Convolve the input without any further complications.
                 output = convolve(input, kernel)
@@ -175,7 +175,7 @@ class Network(object):
         # Convolution for a given input and kernel
         convolve = lambda i, k: tf.nn.atrous_conv2d(i, k, dilation, padding=padding)
         with tf.variable_scope(name) as scope:
-            kernel = self.make_w_var('weights', shape=[k_h, k_w, int(c_i) / group, c_o])
+            kernel = self.make_w_var('weights', shape=[int(k_h), int(k_w), int(c_i) / int(group), int(c_o)])
             if group == 1:
                 # This is the common-case. Convolve the input without any further complications.
                 output = convolve(input, kernel)
---
+++ test.py
@@ -0,0 +1,4 @@
+import cv2
+path = 'output\cihp_edge_maps\\0002190.png'
+img=cv2.imread(path)
+pass
